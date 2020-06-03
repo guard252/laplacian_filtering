b500c5ef11b3dc406a2f2223796bd06fa496c531
guard252 <danylotv43@gmail.com>
2020-06-03 14:47:08 +0300

    Fixed shadows shifting to right-bottom side

--- src/retouch/LaplacianFiltering/LocalLaplacianFilter.cpp
+++ src/retouch/LaplacianFiltering/LocalLaplacianFilter.cpp
@@ -14,14 +14,13 @@ namespace retouch
         LaplacianPyramid output_pyramid(image.getWidth(), image.getHeight(),
                                         image.getChannelsCount(), KPyramid_size);
         output_pyramid.setLayer(KPyramid_size - 1, gaussian_pyramid[KPyramid_size - 1]);
+        RemapFunction remap_function(alpha, beta, sigma);
 
         // hardware_concurrency can return 0
         const size_t number_of_threads = (std::thread::hardware_concurrency() != 0) ?
                 std::thread::hardware_concurrency() : 1;
 
         std::vector<std::thread> working_threads(number_of_threads);
-
-        RemapFunction remap_function(alpha, beta, sigma);
 
         for(int i = 0; i < number_of_threads; i++)
         {
@@ -35,6 +34,11 @@ namespace retouch
             working_threads[i].join();
         }
 
+        for(int i = 0; i < output_pyramid.getLayers().size(); i++)
+        {
+            ImageSaver saver;
+            saver.savePNG(output_pyramid[i], "../images/output_images/lap" + std::to_string(i) + ".png");
+        }
         return output_pyramid.reconstructImage();
     }
 
@@ -49,10 +53,10 @@ namespace retouch
             const size_t KGaussian_width = gaussian_pyramid[layer_number].getWidth();
             const size_t KGaussian_height = gaussian_pyramid[layer_number].getHeight();
 
-            const int KSubimage_size = 3 * ((pow(2, layer_number + 2)) - 1);
+            const int KSubimage_size = 3 * (pow(2, layer_number + 2) - 1);
             const int KSubimage_radius = KSubimage_size / 2;
 
-            for(int y = current_thread; y < KGaussian_height; y+=num_of_threads)
+            for(int y = current_thread; y < KGaussian_height; y += num_of_threads)
             {
                 int y_in_full_resolution = y * pow(2,  layer_number);
                 int y_top_bound = std::max(y_in_full_resolution - KSubimage_radius, 0);
@@ -60,19 +64,21 @@ namespace retouch
                         gaussian_pyramid[0].getHeight());
                 for(int x = 0; x < KGaussian_width; x++)
                 {
-                    std::cout << layer_number << " " << x << " " << y << "\n";
                     int x_in_full_resolution = x * pow(2, layer_number);
-
                     glm::ivec2 subimage_start = {std::max(x_in_full_resolution - KSubimage_radius, 0), y_top_bound};
-                    glm::ivec2 subimage_end = {std::min<int>(x_in_full_resolution + KSubimage_radius, gaussian_pyramid[0].getWidth()), y_bottom_bound};
+                    glm::ivec2 subimage_end = {
+                            std::min<int>(x_in_full_resolution + KSubimage_radius,
+                            gaussian_pyramid[0].getWidth()),
+                            y_bottom_bound };
 
                     Image remapped_subimage = remap_function.remap(gaussian_pyramid[0], subimage_start,
                                                                    subimage_end, gaussian_pyramid[layer_number].getPixel(x, y));
 
-                    LaplacianPyramid temp_laplacian(remapped_subimage);
+                    LaplacianPyramid temp_laplacian(remapped_subimage, layer_number + 2);
 
-                    int new_x = (x_in_full_resolution - subimage_start.x) / pow(2, layer_number);
-                    int new_y = (y_in_full_resolution - subimage_start.y) / pow(2, layer_number);
+
+                    int new_x = (x_in_full_resolution - subimage_start.x) / pow(2, layer_number) + 0.5;
+                    int new_y = (y_in_full_resolution - subimage_start.y) / pow(2, layer_number) + 0.5;
                     Pixel new_pixel = temp_laplacian[layer_number].getPixel(new_x, new_y);
                     output_pyramid.setLayerPixel(layer_number, x, y, new_pixel);
 
